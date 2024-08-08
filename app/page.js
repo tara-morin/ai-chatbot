c504c0c6d1a0a539243e66dcadf8163620ce59c9
tara-morin <tara.v.morin@gmail.com>
2024-08-08 19:47:52 +0000

    made sendmessage helper function

--- app/page.js
+++ app/page.js
@@ -1,14 +1,54 @@
 'use client'
-import { Box, Stack } from "@mui/material";
+import { Box, Stack, TextField, Button} from '@mui/material';
 import Image from "next/image";
 import {useState} from 'react';
 
 export default function Home() {
   const [messages, setMessages]= useState([{
-    role:'assistant', 
+    role:'assistant',
     content:'Hi, I am the Headstarter Support Agent. How can I assist you today?'
-  }])
+  },])
   const[message, setMessage]= useState('')
+
+  //helper funciton to send our current messages array to the backend and get a response
+  const sendMessage= async()=>{
+    setMessage('') //gets message to disapear from textfield after you press send
+    setMessages((messages)=>[
+      ...messages,
+      {role: "user", content: message},
+      {role:"assistant", content: " "}
+    ])
+    const response= fetch('/api/chat',{
+      method:"POST",
+      headers:{
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify([...messages, {role:'user', content:messages}])
+    }).then(async (res)=>{
+      const reader= res.body.getReader()
+      const decoder= new TextDecoder()
+      let result= ''
+      return reader.read().then(function processText({done, value}){
+        if (done){
+          return result
+        }
+        const text= decoder.decode(value || new Int8Array(), {stream:true})
+        //this will either decode the text or will just provide an empty string so our program doesn't crash
+      setMessages((messages)=>{
+        let LastMessage= messages[messages.length-1]
+        let otherMessages= messages.slice(0, messages.length-1)
+        return [
+          ...otherMessages,
+          {
+            ...LastMessage,
+            content: LastMessage.content + text,
+          },
+        ]
+      })
+      return reader.read().then(processText)
+      })
+    })
+  }
 
   return(
     <Box
@@ -26,9 +66,9 @@ export default function Home() {
       border= "1px solid black"
       p={2} spacing={3}>
         <Stack direction="column" spacing= {2} flexGrow={1} overflow="auto" maxHeight= "100%">
-
           {messages.map((message, index)=>(
-              <Box key= {index} display= "flex" justifyContent={
+              <Box key= {index} display= "flex" 
+              justifyContent={
                 message.role==='assistant'? 'flex-start': 'flex-end'}> 
                 {/* makes it so the message goes to one side or the other depending on if its from the chatbot or not */}
                 <Box bgcolor= {
@@ -39,15 +79,14 @@ export default function Home() {
                 </Box>
           ))}
         </Stack>
-        <Stack direction="row" spacing={2} >
-          <Textfield label= "message" 
+        <Stack direction="row" spacing={2}>
+          <TextField 
+          label= "message" 
           fullWidth 
-          value= {message}
-          onChange={(e)=> setMessage(e.target.value)}>
-            <Button variant= "contained">
-              Send
-            </Button>
-          </Textfield>
+          value={message}
+          onChange={(e)=> setMessage(e.target.value)}
+          />
+            <Button variant= "contained">Send</Button>
         </Stack>
       </Stack>
     </Box>
