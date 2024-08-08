653cc9488af4206850313ec84bc258064eb08341
tara-morin <tara.v.morin@gmail.com>
2024-08-08 05:56:50 +0000

    coded route page

--- api/chat/route.js
+++ api/chat/route.js
@@ -8,3 +8,44 @@ Resolve Issues: Address and resolve any technical or account-related issues user
 Provide Resources: Offer helpful information and resources, such as tips for preparing for interviews and explanations of our AI’s evaluation criteria.\
 Maintain Professionalism: Communicate with empathy, patience, and professionalism to ensure a positive user experience.\
 Ensure that all interactions are informative and user-friendly, reflecting HeadstarterAI’s commitment to helping aspiring software engineers succeed."
+
+export async function POST(req){
+    //we are using a post route because we'll be sending out information and expecting stuff back
+    const openai= new OpenAI()
+    const data= await req.json() //gets json data from your request
+    const completion= await openai.chat.completions.create({
+        //chat completion from your request
+        //await ensures your code isn't blocked while waiting for a response, allows multiple requests to be sent at the same time
+        messages: [{
+            role: 'system', content: systemPrompt
+        },
+        ...data, //"spread" operator to get the rest of our message data
+    ],
+    model: "gpt-4o-mini", stream: true,
+    })   
+    //now we need to output this to the front end so we will make a stream response
+
+    const stream= new ReadableStream({
+        async start(controller){
+            const encoder= new TextEncoder()
+            try{
+                for await (const chunk of completion){ //waits for every cpart of the message that completion sends out
+                    const content= chunk.choices[0]?.delta?.content
+                    if (content){ //check if the content exists and if it does, encodes it to send to the controller
+                        const text= encoder.encode(content)
+                        controller.enqueue(text)
+                    }
+                }
+            }
+            catch(error){
+                controller.error(error)
+            }
+            finally{
+                controller.close()
+            }
+        },
+    })
+
+    return new NextResponse(stream)
+    
+}
